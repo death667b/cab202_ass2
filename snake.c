edca4fcf883a890de40717f7e0c8c9c1dcae097a
Ian Daniel <idaniel@spinnys.com>
2016-10-02 09:46:50 +1000

    tails not working correctly

--- snake.c
+++ snake.c
@@ -24,14 +24,14 @@ void setup(void);
 void process_loop(void);
 void display_welcome_screen(void);
 void draw_topbar(void);
-void move_snake(Sprite *snake);
+void move_snake();
 
 int lives = 5;
 int score = 0;
-int move_x = 0;
-int move_y = 0;
+int snake_length = 5;
+int move_x = 0, move_y = 0;
 
-Sprite snake_train;
+Sprite *snake_train = NULL;
 
 
 unsigned char snake_bitmap[] = {0xE0, 0xE0, 0xE0};
@@ -63,36 +63,43 @@ void setup(){
 	// Initialise the LCD screen
 	lcd_init(LCD_DEFAULT_CONTRAST);
 	clear_screen();
-	//display_welcome_screen();
 
-	/*for(int x = 0; x < AREA_X; x++){
-		for(int y = 0; y < AREA_Y; y++){
-			game_area[x][y] = 0;
-		}
-	}*/
+	display_welcome_screen();
 
-
-	init_sprite(&snake_train, 0, 20, 3, 3, snake_bitmap);
+	snake_train = calloc(sizeof(Sprite), snake_length);
+	
+	for (int i = 0; i < snake_length; i++) {
+		init_sprite(&snake_train[i], 20-(i*3), 20, 3, 3, snake_bitmap);
+	}
+	
 }
 
 void process_loop(){
 	clear_screen();
 	
 	draw_topbar();
-	move_snake(&snake_train);
-	draw_sprite(&snake_train);
+	move_snake();
+
+
+
+	for (int i = 0; i < snake_length; i++) {
+		draw_sprite(&snake_train[i]);
+	}
 	show_screen();
 	
 	
 
-	_delay_ms(10);
+	_delay_ms(500);
 
 
 	show_screen();
 }
 
 
-void move_snake(Sprite *snake) {
+void move_snake() {
+	int newx = 0, newy = 0;
+	int oldx = 0, oldy = 0;
+
     // Joystick - Move Up/Down
     if ((PIND >> 1) & 0b1 ){
     	move_x = 0; move_y= -1;
@@ -108,22 +115,48 @@ void move_snake(Sprite *snake) {
     }
 
 	// Test for x axis wall collision
-	if ((snake->x + move_x) == 83) {
-		snake->x = -2;
-	} else if ((snake->x + move_x) == -2) {
-		snake->x = 83;
+	if ((snake_train[0].x + move_x) == 83) {
+		newx = -2;
+	} else if ((snake_train[0].x + move_x) == -2) {
+		newx = 83;
 	} else {
-		snake->x += move_x;
+		newx = snake_train[0].x + move_x;
 	}
 
 	// Test for y axis wall collision
-	if ((snake->y + move_y) == 46) {
-		snake->y = -2;
-	} else if ((snake->y + move_y) == -2) {
-		snake->y = 46;
+	if ((snake_train[0].y + move_y) == 46) {
+		newy = -2;
+	} else if ((snake_train[0].y + move_y) == -2) {
+		newy = 46;
 	} else {
-		snake->y += move_y;
+		newy = snake_train[0].y + move_y;
 	}
+
+	int addx = 0, addy = 0;
+	// Move Tail
+	if ( move_y != 0 || move_x != 0 ){
+		for (int i = 0; i < snake_length; i++) {
+		    oldx = snake_train[i].x;
+		    oldy = snake_train[i].y;
+	
+		    if (i != 0) {
+		    	addx = move_x *-2;
+		    	addy = move_y *-2;
+		    }
+
+		    snake_train[i].x = newx + addx;
+		    snake_train[i].y = newy + addy;
+	
+		    newx = oldx;
+		    newy = oldy;
+		}
+	}
+
+
+
+
+
+
 
 
 }
@@ -132,8 +165,7 @@ void display_welcome_screen(){
 	draw_string(42-25,15, "Ian Daniel");
 	draw_string(42-20,25, "n5372828");
 	show_screen();
-	_delay_ms(500);
-	///////////////////////////////////////////////  TODO - Change to 2 seconds
+	_delay_ms(300);
 }
 
 void draw_topbar(){
