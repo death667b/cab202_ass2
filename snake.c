f0ccf5427adfd0c2e0ae677e4de14b0dd2a6c614
Ian Daniel <idaniel@spinnys.com>
2016-10-09 17:12:28 +1000

    working wall movenment, food eating and tail growth

--- snake.c
+++ snake.c
@@ -36,16 +36,18 @@ void setup(void);
 void process_loop(void);
 void display_welcome_screen(void);
 void draw_topbar(void);
-void move_snake();
-void restart_round();
-void create_food();
-void create_snake_train();
-int test_tail_collision();
+void move_snake(void);
+void restart_round(void);
+void create_food(void);
+void food_eaten_test(void);
+void create_snake_train(void);
+void grow_snake_train(void);
+int collision_test(void);
 
 int lives = 5;
 int score = 0;
 int snake_length = 10;
-int start_x = 40, start_y = 21;
+int start_x = 39, start_y = 21;
 
 volatile unsigned char btn_hists[NUM_BUTTONS];
 volatile unsigned char btn_states[NUM_BUTTONS];
@@ -112,7 +114,7 @@ void process_loop(){
 	draw_topbar();
 	draw_sprite(&chow_time);
 	move_snake();
-	round_over = test_tail_collision();
+	round_over = collision_test();
 
 
 	for (int i = 0; i < snake_length; i++) {
@@ -126,17 +128,41 @@ void process_loop(){
 	}
 	
 
-	_delay_ms(250);
+	_delay_ms(350);
 
 
 
 }
 
 void create_food(){
+	int x = rand() % 82;
+	int y = rand() % 40;
 
+	while (x <= 30 && y <= 8){
+		x = rand() % 82;
+		y = rand() % 40;
+	}
 
+	init_sprite(&chow_time, x, y, 3, 3, food_bitmap);
+}
 
-	init_sprite(&chow_time, rand() % 84, rand() % 42, 3, 3, food_bitmap);
+void food_eaten_test(){
+	int sx = snake_train[0].x, sy = snake_train[0].y;
+	int fx = chow_time.x, fy = chow_time.y;
+
+	if (((fx <= sx+2 && fx >= sx) || (sx <= fx+2 && sx >= fx)) && 
+		((fy <= sy+2 && fy >= sy) || (sy <= fy+2 && sy >= fy))) {
+		score++;
+		grow_snake_train();
+
+		chow_time.x = rand() % 82;
+		chow_time.y = rand() % 40;
+
+		while (chow_time.x <= 30 && chow_time.y <= 8){
+			chow_time.x = rand() % 82;
+			chow_time.y = rand() % 40;
+		}
+	}
 }
 
 
@@ -144,19 +170,21 @@ void move_snake() {
 	int newx = 0, newy = 0;
 	int oldx = 0, oldy = 0;
 
+	score = snake_train[0].y;
+
 	// Test for x axis wall collision
-	if ((snake_train[0].x + move_x) >= 81) {
+	if ((snake_train[0].x + move_x) >= 82) {
 		newx = 0;
-	} else if ((snake_train[0].x + move_x) <= 0) {
+	} else if ((snake_train[0].x + move_x) <= -1) {
 		newx = 81;
 	} else {
 		newx = snake_train[0].x + (move_x*3);
 	}
 
 	// Test for y axis wall collision
-	if ((snake_train[0].y + move_y) >= 45) {
+	if ((snake_train[0].y + move_y) >= 46) {
 		newy = 0;
-	} else if ((snake_train[0].y + move_y) <= 0) {
+	} else if ((snake_train[0].y + move_y) <= -1) {
 		newy = 45;
 	} else {
 		newy = snake_train[0].y + (move_y*3);
@@ -184,8 +212,18 @@ void restart_round(){
 	move_x = 0;
 	move_y = 0;
 
-	snake_length = 10;
+	snake_length = 2;
 	create_snake_train();
+}
+
+void grow_snake_train(){
+	int x = snake_train[snake_length-1].x;
+	int y = snake_train[snake_length-1].y;
+
+	snake_length++;
+	snake_train = realloc(snake_train, snake_length * sizeof(Sprite));
+	
+	init_sprite(&snake_train[snake_length-1], x, y, 3, 3, snake_bitmap);
 }
 
 
@@ -198,42 +236,31 @@ void create_snake_train(){
 }
 
 
-int test_tail_collision(){
+int collision_test(){
 	int x = snake_train[0].x;
 	int y = snake_train[0].y;
-	int collision = FALSE;
+	int round_over = FALSE;
 
 
 	for (int i = 1; i < snake_length; i++) {
-		if (move_x == 0 && move_y == -1){ // Moving Up
-			if ((snake_train[i].x == x+0 && snake_train[i].y == y+0) ||
-					(snake_train[i].x == x+1 && snake_train[i].y == y+0) ||
-					(snake_train[i].x == x+2 && snake_train[i].y == y+0)) {
-				collision = TRUE;
-			}
-		} else if (move_x == 0 && move_y == 1){ // Moving Down
-			if ((snake_train[i].x == x+0 && snake_train[i].y == y+0) ||
-					(snake_train[i].x == x+1 && snake_train[i].y == y+0) ||
-					(snake_train[i].x == x+2 && snake_train[i].y == y+0)) {
-				collision = TRUE;
-			}
-		} else if (move_x == -1 && move_y == 0){ // Moving Left
-			if ((snake_train[i].x == x+0 && snake_train[i].y == y+0) ||
-					(snake_train[i].x == x+0 && snake_train[i].y == y+1) ||
-					(snake_train[i].x == x+0 && snake_train[i].y == y+2)) {
-				collision = TRUE;
-			}
-		} else if (move_x == 1 && move_y == 0){ // Moving Right
-			if ((snake_train[i].x == x+0 && snake_train[i].y == y+0) ||
-					(snake_train[i].x == x+0 && snake_train[i].y == y+1) ||
-					(snake_train[i].x == x+0 && snake_train[i].y == y+2)) {
-				collision = TRUE;
-			}
+		food_eaten_test();
 
+		if (move_x == 0 && (move_y == -1 || move_y == 1)){ // Moving Up or Down
+			if ((snake_train[i].x == x+0 && snake_train[i].y == y+0) ||
+					(snake_train[i].x == x+1 && snake_train[i].y == y+0) ||
+					(snake_train[i].x == x+2 && snake_train[i].y == y+0)) {
+				round_over = TRUE;
+			}
+		} else if ((move_x == -1 || move_x == 1) && move_y == 0){ // Moving Left or Right
+			if ((snake_train[i].x == x+0 && snake_train[i].y == y+0) ||
+					(snake_train[i].x == x+0 && snake_train[i].y == y+1) ||
+					(snake_train[i].x == x+0 && snake_train[i].y == y+2)) {
+				round_over = TRUE;
+			}
 		}
 	}
 
-	return collision;
+	return round_over;
 }
 
 void display_welcome_screen(){
